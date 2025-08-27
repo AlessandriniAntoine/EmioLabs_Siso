4b5e75d38eb1c1389e4566f758ac5325bbc1810c
Alessandrini Antoine <a.alsdrini@gmail.com>
2025-08-27 17:14:23 +0200

    split new gui data

--- scripts/closedLoopController.py
+++ scripts/closedLoopController.py
@@ -5,35 +5,9 @@ ControlMode = {"Open Loop": False, "State Feedback": True}
 
 
 class ClosedLoopController(BaseController):
-    def __init__(self, leg, motor, markers, load, motorInit, motorMin, motorMax, cutoffFreq, order, useObserver=1):
+    def __init__(self, leg, motor, markers, load, motorInit, motorMin, motorMax, cutoffFreq, order, use_observer=1):
         super().__init__(leg, motor, markers, load, motorInit, motorMin, motorMax, cutoffFreq)
 
-        # Specific gui setup
-        if useObserver:
-            self.guiNode.addData(name="noise", type="float", value=0.)
-        self.guiNode.addData(name="reference", type="float", value=0.)
-        self.guiNode.addData(name="output", type="float", value=0.)
-        self.guiNode.addData(name="controlMode", type="bool", value=ControlMode["Open Loop"])
-        MyGui.MyRobotWindow.addSettingInGroup("Reference (mm)", self.guiNode.reference, -50, 50, "Control Law")
-        if useObserver:
-            MyGui.MyRobotWindow.addSettingInGroup("Observer Noise (mm)", self.guiNode.noise, 0, 3, "Control Law")
-        MyGui.MyRobotWindow.addSettingInGroup("Control Mode", self.guiNode.controlMode, 0, 1, "Buttons")
-
-        # Plotting data
-        MyGui.PlottingWindow.addData("Reference", self.guiNode.reference)
-        MyGui.PlottingWindow.addData("Output", self.guiNode.output)
-
-        # Control and Observer data
-        model = np.load(os.path.join(data_path, f"model_order{order}.npz"))
-        self.A, self.B, self.C = model["stateMatrix"], model["inputMatrix"], model["outputMatrix"]
-        control = np.load(os.path.join(data_path, f"controller_order{order}.npz"))
-        self.K, self.G = control["feedbackGain"], control["feedForwardGain"]
-        if useObserver:
-            observer = np.load(os.path.join(data_path, f"observer_order{order}.npz"))
-            self.L = observer["observerGain"]
-        else:
-            reduction = np.load(os.path.join(data_path, f"reduction_order{order}.npz"))
-            self.R = reduction["reductionMatrix"]
 
         # add mechanical object for reference
         self.refMo = self.guiNode.addObject("MechanicalObject",
@@ -46,19 +20,31 @@ class ClosedLoopController(BaseController):
             showColor=[0, 0, 1, 1]
         )
 
-        self.setup_additional_variables(useObserver)
+        self.setup_additional_variables(use_observer, order)
+        self.setup_additional_gui(use_observer)
 
 
-    def setup_additional_variables(self, useObserver):
-
-        # additional constant
-        self.useObserver = useObserver
+    def setup_additional_variables(self, use_observer, order):
+        # Control and Observer data
+        model = np.load(os.path.join(data_path, f"model_order{order}.npz"))
+        self.A, self.B, self.C = model["stateMatrix"], model["inputMatrix"], model["outputMatrix"]
+        control = np.load(os.path.join(data_path, f"controller_order{order}.npz"))
+        self.K, self.G = control["feedbackGain"], control["feedForwardGain"]
+        if use_observer:
+            observer = np.load(os.path.join(data_path, f"observer_order{order}.npz"))
+            self.L = observer["observerGain"]
+        else:
+            reduction = np.load(os.path.join(data_path, f"reduction_order{order}.npz"))
+            self.R = reduction["reductionMatrix"]
 
         # additional states for closed-loop control
         self.reference = np.zeros((self.B.shape[1], 1))
         self.observerState = np.zeros((self.A.shape[0], 1))
         self.observerOutput = np.zeros((self.C.shape[0], 1))
         self.measurePrev = np.zeros((self.C.shape[0], 1))
+
+        # additional constant
+        self.use_observer = use_observer
 
         # additional data storage
         self.observerStateList = []
@@ -68,7 +54,7 @@ class ClosedLoopController(BaseController):
 
     def execute_control_at_camera_frame(self):
         # observer
-        if self.useObserver:
+        if self.use_observer:
             cmd = self.currentMotorPos.reshape(-1, 1)
             self.observerOutput = self.C @ self.observerState
             measureNoised = self.measurePrev + np.random.normal(0, self.guiNode.noise.value, self.markersPos.shape)
@@ -83,7 +69,7 @@ class ClosedLoopController(BaseController):
 
         # control
         if self.guiNode.controlMode.value == ControlMode["State Feedback"]:
-            if not self.useObserver:
+            if not self.use_observer:
                 fullState = np.vstack([self.legVel, self.legPos])
                 state4Control = self.R.T @ fullState
             desiredMotorPos = ( self.G @ self.reference - self.K @ state4Control).flatten()
@@ -101,6 +87,23 @@ class ClosedLoopController(BaseController):
     def execute_control_at_simu_frame(self):
         super().execute_control_at_simu_frame()
         self.guiNode.output.value = self.markersPos[1, 0]
+
+
+    def setup_additional_gui(self, use_observer):
+        # Specific gui setup
+        if use_observer:
+            self.guiNode.addData(name="noise", type="float", value=0.)
+        self.guiNode.addData(name="reference", type="float", value=0.)
+        self.guiNode.addData(name="output", type="float", value=0.)
+        self.guiNode.addData(name="controlMode", type="bool", value=ControlMode["Open Loop"])
+        MyGui.MyRobotWindow.addSettingInGroup("Reference (mm)", self.guiNode.reference, -50, 50, "Control Law")
+        if use_observer:
+            MyGui.MyRobotWindow.addSettingInGroup("Observer Noise (mm)", self.guiNode.noise, 0, 3, "Control Law")
+        MyGui.MyRobotWindow.addSettingInGroup("Control Mode", self.guiNode.controlMode, 0, 1, "Buttons")
+
+        # Plotting data
+        MyGui.PlottingWindow.addData("Reference", self.guiNode.reference)
+        MyGui.PlottingWindow.addData("Output", self.guiNode.output)
 
 
     def record_data(self):
